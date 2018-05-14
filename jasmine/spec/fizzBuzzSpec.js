3c63ec1c0ff7337d9c6b02076180d5b455bce3bd
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-14 16:47:24 +0100

    Conditions met

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -2,4 +2,12 @@ describe("fizzBuzz", function() {
   it ("prints fizz", function() {
     expect(fizzBuzz(3)).toEqual("Fizz");
   });
+
+  it ("prints Buzz", function() {
+    expect(fizzBuzz(5)).toEqual("Buzz");
+  });
+
+  it ("Prints Fizzbuzz", function () {
+    expect(fizzBuzz(15)).toEqual("FizzBuzz");
+  });
 });
